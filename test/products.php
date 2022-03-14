12346afaef430ed1ec6454e9f066c48f05e03762
szejin00 <87605188+szejin00@users.noreply.github.com>
2022-03-14 16:58:41 +0800

    Add files via upload

---
+++ test/products.php
@@ -0,0 +1,19 @@
+<?php
+	$products = array(
+		"product1" => array(
+			"title" => "My Amazing Product 1",
+			"price" => 6700,
+			"features" => array("feature 1", "feature 2", "feature 3")
+		),
+		"product2" => array(
+			"title" => "My Amazing Product 2",
+			"price" => 14700,
+			"features" => array("feature 1", "feature 2", "feature 3")
+		),
+		"product3" => array(
+			"title" => "My Amazing Product 3",
+			"price" => 3700,
+			"features" => array("feature 1", "feature 2", "feature 3")
+		)
+	);
+?>
